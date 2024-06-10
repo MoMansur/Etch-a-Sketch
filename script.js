b4d916a522f06c9d18fa4165813495adce9e4e17
MoMansur <mansurshehumsm@gmail.com>
2024-06-10 19:29:06 +0300

    Send

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container =  document.getElementsByClassName('container')
 const gridBox = document.getElementById('gridBox')
 
 const sizeBtn = document.getElementById('size')
+const resetBtn = document.getElementById('resetBtn')
 
 const colorInput = document.getElementById('ColorInput');
 
@@ -16,6 +17,9 @@ const gridContainer = document.getElementById('gridContainer');
 const generateGridButton = document.getElementById('generateGridButton');
 const squaresPerSideInput = document.getElementById('squaresPerSide');
 
+
+
+
 function createGrid() {
     
     gridContainer.innerHTML = '';
@@ -23,17 +27,86 @@ function createGrid() {
    
     const squaresPerSide = squaresPerSideInput.value;
     document.documentElement.style.setProperty('--squares-per-side', squaresPerSide);
-
+    
     // Create squares
     for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
         gridContainer.appendChild(square);
 
-        colorInput.addEventListener('change', ()=>{
-            square.style.backgroundColor = colorInput.value
-        })
+
+        function theHover(color){
+            square.style.backgroundColor = color
+        }
+        let isClicked= true
+
+        function squareEvent(color){
+
+            
+
+            square.addEventListener('mouseover', ()=>{
+
+                if(isClicked){
+                    theHover(color)
+                }else{
+
+                }
+                isClicked = !isClicked
+            })
+         
+          
+        }
+        
+        function reset(){
+            resetBtn.addEventListener('click', ()=>{
+                square.style.backgroundColor = ''
+            })
+        }
+        reset()
+
+
+
+        function colorEvent(){
+            let color = colorInput.value
+
+            colorInput.addEventListener('change', ()=>{
+                let color = colorInput.value
+                squareEvent(color)
+                gridBoxEvent()
+                      
+            }) 
+            gridBoxEvent(color)
+   
+        }
+
+        colorEvent()
+
+        let clickCount = 0;
+        function gridBoxEvent(color){
+
+
+            gridContainer.addEventListener('click', ()=>{
+ 
+                squareEvent(color)
+
+        
+            })
+
+            gridContainer.addEventListener('dblclick', ()=>{
+                square.style.backgroundColor = ''
+                
+            })
+        }
+
+        function draw(){
+            gridBoxEvent()
+        
+        }
+
     }
+
+
+
 }
 
 sizeRange.addEventListener('input', ()=>{
@@ -41,6 +114,7 @@ sizeRange.addEventListener('input', ()=>{
 })
 
 
+createGrid()
 
 
 generateGridButton.addEventListener('click', createGrid);
